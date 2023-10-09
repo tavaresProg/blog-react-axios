7059f0faec9313893aa520da26d0360bd2400994
Arthur <arthurleonel7@gmail.com>
2023-10-09 19:12:08 -0300

    API fetch completed

--- react-axios-blog/src/routes/Home.jsx
+++ react-axios-blog/src/routes/Home.jsx
@@ -1,6 +1,49 @@
+import axios from "axios"
+
+import { useState, useEffect } from "react"
+
+import { Link } from "react-router-dom"
+
+import "./Home.css"
+
 const Home = () => {
+
+
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+
+        try {
+
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+            const data = response.data
+            setPosts(data)
+
+        } catch (error) {
+            console.log("error")
+        }
+
+
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
+
+
     return (
-        <div>Home</div>
+        <div>
+            <h1>Ultimos posts</h1>
+            {posts.length === 0 ? <p>Carregando...</p> : (
+                posts.map((post) => (
+                    <div className="post" key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <Link to={(`/posts/${post.id}`)} className="btn">Ler mais</Link>
+                    </div>
+                ))
+            )}
+        </div>
     )
 }
 
