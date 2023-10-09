4a377181d62f3d84352456f2c9508dc9561646dc
Arthur <arthurleonel7@gmail.com>
2023-10-09 15:39:22 -0300

    configured react router dom

--- react-axios-blog/src/App.jsx
+++ react-axios-blog/src/App.jsx
@@ -1,8 +1,12 @@
+import { Outlet } from "react-router-dom"
+
 function App() {
 
   return (
     <>
-      <h1>React Axios</h1>
+      <div className="container">
+        <Outlet />
+      </div>
     </>
   )
 }
--- react-axios-blog/src/main.jsx
+++ react-axios-blog/src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
 import './index.css'
+
+//pages
+import Home from "./routes/Home"
+import NewPost from "./routes/NewPost"
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      }
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
