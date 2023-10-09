56575d4e540dcedb2c701eca65272158d29c728e
Arthur <arthurleonel7@gmail.com>
2023-10-09 18:53:24 -0300

    based styles added

--- react-axios-blog/src/App.jsx
+++ react-axios-blog/src/App.jsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom"
 
+import  "./App.css";
+
+import Navbar from "../src/components/Navbar"
+
 function App() {
 
   return (
-    <>
-      <div className="container">
-        <Outlet />
-      </div>
-    </>
+    
+    <div className="App">
+      <Navbar/>
+        <div className="container">
+          <Outlet />
+        </div>
+    </div>
+    
   )
 }
 
